542ee5f99b0ac8d7b25d0217d4d125323616b7ae
Daniel Gómez <88523667+Danigom11@users.noreply.github.com>
2022-11-19 22:56:13 +0100

    initial

--- Día 5/Ejercicio_4.py
+++ Día 5/Ejercicio_4.py
@@ -1,17 +1,20 @@
-def contar_primos(numero):
-    rango = range(2, numero)
-    cantidad_primos = 0
-    for num in rango:
-        for num2 in range(2, num):
-            contador_primo = 0
-            if num % num2 == 0:
-                contador_primo += 1
-            if contador_primo == 2:
-                break
-            else:
-                cantidad_primos += 1
+def numero_primo(numero):
+    numeros_dentro_de_numero = range(2, numero)
+    contador = 0
+    for num in numeros_dentro_de_numero:
+        if numero % num == 0:
+            contador += 1
+            return False
+    return True
 
+
+def contar_primos(numero):
+    cantidad_primos = 0
+    rango_busqueda_primos = range(2, numero)
+    for numero_busqueda in rango_busqueda_primos:
+        if numero_primo(numero_busqueda):
+            cantidad_primos += 1
     return cantidad_primos
 
 
-print(contar_primos(5))
+print(contar_primos(7))
